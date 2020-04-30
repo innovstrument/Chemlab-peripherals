61a0f0ada386454050a16d3939c56e8ee308e235
sinuoc <45698324+sinuoc@users.noreply.github.com>
2020-04-29 23:07:13 -0700

    Update arduino-uno-control.ino
    init mod

--- Rotavap/Pressure-control/arduino-uno-control.ino
+++ Rotavap/Pressure-control/arduino-uno-control.ino
@@ -1,13 +1,13 @@
-int ledPin = 13;  // LED connected to digital pin 13
-int inPin = 7;    // pushbutton connected to digital pin 7
+int MotorPin = 13;  // Motor driver connected to digital pin 13
+int GaugePin = 7;    // Gauge reading connected to digital pin 7
 int val = 0;      // variable to store the read value
 
 void setup() {
-  pinMode(ledPin, OUTPUT);  // sets the digital pin 13 as output
-  pinMode(inPin, INPUT);    // sets the digital pin 7 as input
+  pinMode(MotorPin, OUTPUT);  // sets the digital pin 13 as output
+  pinMode(GaugePin, INPUT);    // sets the digital pin 7 as input
 }
 
 void loop() {
-  val = digitalRead(inPin);   // read the input pin
-  digitalWrite(ledPin, val);  // sets the LED to the button's value
+  val = digitalRead(GaugePin);   // read the Pressure gauge metric input
+  digitalWrite(MotorPin, val);  // sets the LED to the button's value
 }
