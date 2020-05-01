b26e4dbaee76296188dae7cb8450ab1382a7b630
sinuoc <45698324+sinuoc@users.noreply.github.com>
2020-04-30 23:12:09 -0700

    Update arduino-uno-control.ino

--- Rotavap/Pressure-control/arduino-uno-control.ino
+++ Rotavap/Pressure-control/arduino-uno-control.ino
@@ -1,13 +1,19 @@
-int MotorPin = 13;  // Motor driver connected to digital pin 13
-int GaugePin = 7;    // Gauge reading connected to digital pin 7
-int val = 0;      // variable to store the read value
+int MotorPin = 13;  // 马达驱动由数字的高低电平输出控制，这里设置为#13针
+int GaugePin = A5;    // 真空压力变送器（真空规）输出电压连接到#A5针，注意要和单片机共地。
+int GaugeValue = 0;      // 初始化真空规读值为0
 
 void setup() {
-  pinMode(MotorPin, OUTPUT);  // sets the digital pin 13 as output
-  pinMode(GaugePin, INPUT);    // sets the digital pin 7 as input
+  pinMode(MotorPin, OUTPUT);  // 初始化输出
+  pinMode(GaugePin, INPUT);    // 初始化输入
 }
 
 void loop() {
-  val = digitalRead(GaugePin);   // read the Pressure gauge metric input
-  digitalWrite(MotorPin, val);  // sets the LED to the button's value
+  GaugeValue = analogRead (GaugePin);  //Ardunio Uno配有6路模拟输入，分别标记为A0到A5。每路输入提供10位分辨率（即2^10，1024位不同的值）。虽然可以通过AREF引脚和模拟参考()功能来改变模拟输入的上限值，但其默认输入值范围为0-5V。
+  float voltage = GaugeValue * (5.0 / 1023.0); //显示电压
+  Serial.println(voltage); //显示电压
+  if (GaugeValue >= (5.0 / (101325.0 * 0.8) ) //如果读取电压数值低于0.8倍大气压
+  {
+    digitalWrite(MotorPin,LOW)  //给电机控制写低电平，关断继电器
+  }
 }
+
